c6e35b90c646841574390647bc56cccc3094e354
Delahaye Marien <delahaye.marien@gmail.com>
2017-09-29 12:00:00 +0200

    Edited main.css and created create_project.php

--- create_project.php
+++ create_project.php
@@ -1,12 +1,15 @@
 <?php
 include 'header.php';
 ?>
+<center><div class="myform">
 <form action="" method="post">
     <input type="text"  id="project_title" placeholder="Project Name"><br>
     <input type="text"  id="employees" placeholder="Employees who work on the project"><br>
     <textarea name="Task to do" id="" cols="30" rows="10" placeholder="Task and step to do"></textarea><br>
     <input type="submit" class="btn btn-default">
 </form>
+</center>
+</div>
 <?php
 include 'footer.php';
 
