8d4b27d190ed6210a20e877ba5192de629fa9d6e
Delahaye Marien <delahaye.marien@gmail.com>
2017-09-29 10:51:39 +0200

    First commit

---
+++ create_project.php
@@ -0,0 +1,13 @@
+<?php
+include 'header.php';
+?>
+<form action="" method="post">
+    <input type="text"  id="project_title" placeholder="Project Name"><br>
+    <input type="text"  id="employees" placeholder="Employees who work on the project"><br>
+    <textarea name="Task to do" id="" cols="30" rows="10" placeholder="Task and step to do"></textarea><br>
+    <input type="submit" class="btn btn-default">
+</form>
+<?php
+include 'footer.php';
+
+    
---
+++ header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<header>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">BTP Moreau</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Login</a></li>
+      <li><a href="create_project.php">Create Project</a></li>
+
+    </ul>
+  </div>
+</nav>
+</header>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>BTP Moreau</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
