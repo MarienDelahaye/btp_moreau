ef3bb7d156d2cad275dba0465298a51c2c960a57
Delahaye Marien <delahaye.marien@gmail.com>
2017-10-02 14:28:56 +0200

    Worked on project_panel.php and created project_panel_post.php for the post request

--- project_panel.php
+++ project_panel.php
@@ -8,13 +8,7 @@ include 'header.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Project Panel</title>
-</head>
-<body>
-</body>
-</html>
-
-
-<?php
+    <?php
 include 'database.php';
 include 'footer.php';
 
@@ -31,3 +25,21 @@ $project = $dropdown->fetchAll();
 <option value="<?php echo $projects['id'];?>"><?php echo $projects['project_title']; ?></option>
 <?php endforeach; ?>
 </select>
+</head>
+<body>
+<center><div class="panel panel-default">
+<form class="panel-body" action="project_panel_post.php" method="post">
+    <input type="text" name="project_title" placeholder="New Project Name"><br>
+    <input type="text" name="user" placeholder="Update employees who work on it"><br>
+    <input type="text" name="deadline" placeholder="New Deadline"><br>
+    <textarea name="task"  cols="30" rows="10" placeholder="New Task and step to do"></textarea><br>
+    <textarea name="description" cols="30" rows="10" placeholder="New Description of the project"></textarea><br>
+    <input type="submit" class="btn btn-default">
+</form>
+</center>
+</div>
+</body>
+</html>
+
+
+
--- project_panel_post.php
+++ project_panel_post.php
@@ -1,14 +1,16 @@
 <?php
 include 'database.php';
+include 'header.php';
+include 'footer.php';
 ?>
-
 <?php
+
 $project_title = $_POST['project_title'];
 $user = $_POST['user'];
 $task = $_POST['task'];
 $deadline = $_POST['deadline'];
 $description = $_POST['description'];
-$req = $db->prepare('INSERT INTO db_btp(project_title, user, task, deadline, description) VALUES(:project_title, :user, :task, :deadline, :description)');
+$req = $db->prepare ('UPDATE db_btp SET project_title = :project_title, user = :user, task = :task, deadline = :deadline, description = :description');
 $req->execute(array(
     'project_title' => $_POST['project_title'],
     'user' => $_POST['user'],
@@ -17,6 +19,4 @@ $req->execute(array(
     'description' => $_POST['description']
 ));
 ?>
-<p>The project <?php echo $_POST['project_title']; ?> has been created ! </p>
-<a href="project_panel.php">Manage projects</a><br>
-<a href="index.php">Comeback to main page</a>
+<p>The project <?php echo $_POST['project_title']; ?> has been updated ! </p>
