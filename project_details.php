011b694269be7b61b5a9b3058bf12ad2f5e72a16
Delahaye Marien <delahaye.marien@gmail.com>
2017-10-03 09:07:30 +0200

    Adding modification, wireframe and usecase

--- project_details.php
+++ project_details.php
@@ -1,17 +1,22 @@
 <?php
 include 'header.php'; // Include my header
-?>
-<table style="width:100%">
-<tr>
-<th>Task name:</th>
-<th>Deadline:</th>
-<th>Employees:</th>
-</tr>
-<tr>
-<td>Building an house</td>
-<td>3 weeks</td>
-<td>?</td>
-</tr>
-<?php
 include 'footer.php'; // Include my footer
+include 'database.php';
+?>
+<?php
+$req = $db->query('SELECT * FROM db_btp');
+
+$data = $req->fetchAll();
+{
+?>
+
+<strong>Project Name</strong> : <?php echo $data['project_title']; ?><br />
+<strong>Employees</strong: <?php echo $data['user']; ?><br />
+
+
+<?php
+}
+$info->closecursor();
+
+
 ?>
