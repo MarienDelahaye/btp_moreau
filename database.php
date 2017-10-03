7cbf10f73a67928e4b476858267bb3a96b14a337
Delahaye Marien <delahaye.marien@gmail.com>
2017-10-03 11:55:41 +0200

    Removed my password on database.php

--- database.php
+++ database.php
@@ -1,2 +1,2 @@
 <?php
-$db = new PDO('mysql:host=localhost;dbname=btpmoreau;charset=utf8', 'root', 'marien_delahaye', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$db = new PDO('mysql:host=localhost;dbname=btpmoreau;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
